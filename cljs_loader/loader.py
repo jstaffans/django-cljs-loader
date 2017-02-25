80b43df8e8408c26437971975e155cda2c2e0db7
Johannes Staffans <johannes.staffans@artory.com>
2017-02-25 16:26:10 +0100

    Fish out output bundles

--- cljs_loader/loader.py
+++ cljs_loader/loader.py
@@ -1,5 +1,7 @@
 from django.conf import settings
+from django.core.exceptions import ImproperlyConfigured
 import edn_format
+from edn_format.edn_lex import Keyword
 import re
 
 
@@ -11,13 +13,38 @@ def _strip_meta(raw):
     return re.sub(r'\^{.*?}', '', raw)
 
 
-def get_bundle():
+def _get_output_to(build_config):
+    return build_config.get(Keyword('compiler'), {}).get(Keyword('output-to'))
+
+def _get_output_bundles(builds):
+    bundles = {}
+
+    if type(builds) is edn_format.immutable_dict.ImmutableDict:
+        for id, build_config in builds.items():
+            bundles[id.name] = _get_output_to(build_config)
+    else:
+        for build_config in builds:
+            bundles[build_config.get(Keyword('id'))] = _get_output_to(build_config)
+
+    return bundles
+
+
+def get_bundles():
     with open(settings.CLJS_LOADER['PROJECT_FILE'], 'r') as project_file:
         raw = ''.join(_remove_line_comments(project_file.readlines()))
 
     content = _strip_meta(raw)
+
     project_settings = edn_format.loads(content)
 
-    # TODO: fish out output bundle 
+    key = Keyword('cljsbuild')
+    if key in project_settings:
+        cljsbuild = project_settings[project_settings.index(key) + 1]
+    else:
+        raise ImproperlyConfigured(':cljsbuild key not found in Leiningen project settings!')
 
-    return project_settings
+    output_bundles = _get_output_bundles(cljsbuild.get(Keyword('builds')))
+
+    return output_bundles
+
+    # return [_bundle_name(b) for b in output_bundles]
