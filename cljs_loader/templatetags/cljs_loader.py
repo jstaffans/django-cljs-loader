e7752962e3ef189b52e48f720c22ae14b3cdbc05
Johannes Staffans <johannes.staffans@artory.com>
2017-05-11 23:15:04 +0200

    Simplify api

--- cljs_loader/templatetags/cljs_loader.py
+++ cljs_loader/templatetags/cljs_loader.py
@@ -1,4 +1,5 @@
 from django import template
+from django.conf import settings
 from django.utils.safestring import mark_safe
 
 from cljs_loader import loader, utils
@@ -8,7 +9,8 @@ register = template.Library()
 ldr = loader.Loader()
 
 @register.simple_tag
-def render_bundle(bundle_name):
+def render_bundle():
+    bundle_name = settings.CLJS_LOADER['CLJS_BUILD'].name
     bundle = ldr.get_bundle(bundle_name)
     tag = utils.to_tag(bundle)
     return mark_safe(tag)
