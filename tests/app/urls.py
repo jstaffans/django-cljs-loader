37604a3c3c8c736dd2b53beca10ed3f3697dd4a1
Johannes Staffans <johannes.staffans@gmail.com>
2017-12-21 14:10:47 +0100

    Django 2.0 compatibility

--- tests/app/urls.py
+++ tests/app/urls.py
@@ -17,8 +17,8 @@ from django.conf.urls import include, url
 from django.contrib import admin
 from django.views.generic import TemplateView
 
+app_name = 'app'
 
 urlpatterns = [
     url(r'^$', TemplateView.as_view(template_name='home.html'), name='home'),
-    url(r'^admin/', include(admin.site.urls)),
 ]
